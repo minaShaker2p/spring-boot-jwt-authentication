78844b2b8ff640ec632e00223c84605096280f99
Mina Rezkalla <minashakeralfy@gmail.com>
2020-10-26 11:18:14 +0100

    initial commit

---
+++ src/test/java/com/mina/springbootjwtauthentication/SpringBootJwtAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mina.springbootjwtauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
