4fd5994609bbd6b8598d480afe3a488157edc5d1
Mina Rezkalla <minashakeralfy@gmail.com>
2020-10-26 12:13:13 +0100

    create role repository

--- src/main/java/com/mina/springbootjwtauthentication/repository/RoleRepository.java
+++ src/main/java/com/mina/springbootjwtauthentication/repository/RoleRepository.java
@@ -1,5 +1,15 @@
 package com.mina.springbootjwtauthentication.repository;
 
 
-public interface RoleRepository {
+import com.mina.springbootjwtauthentication.model.ERole;
+import com.mina.springbootjwtauthentication.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    Optional<Role> findByName(ERole role);
 }
